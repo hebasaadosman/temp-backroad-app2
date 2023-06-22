8a218ef7c6b9f45f93650d6a030caa510b450bc0
hebaosman443 <hebaosman443@gmail.com>
2023-06-23 01:20:42 +0300

    first commit

---
+++ src/components/PageLinks.js
@@ -0,0 +1,11 @@
+import { pageLinks } from "../data";
+import PageLink from "./PageLink";
+export const PageLinks = ({parentClass,itemClass}) => {
+  return (
+    <ul className={parentClass} id="nav-links">
+      {pageLinks.map((link) => {
+        return <PageLink key={link.id} {...link} itemClass={itemClass} />
+      })}
+    </ul>
+  )
+}
